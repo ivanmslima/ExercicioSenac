8beb4b381e2ffa2ef65e50b60add88b5c91ff665
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-30 17:52:55 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioSenac/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            String nome, senha;
+
+            Console.WriteLine("Digite o nome do usuário: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a senha do usuário");
+            senha = Console.ReadLine();
+
+            if(nome == "Ivan" && senha == "123")
+            {
+                Console.WriteLine("Acesso liberado");
+            }
+            else
+            {
+                Console.WriteLine("Acesso negado");
+            }
+
+
+
+
+        }
+    }
+}
